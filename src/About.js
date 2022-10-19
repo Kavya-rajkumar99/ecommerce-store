da0b8de6688d2bfc571a88ff4625a4fb36c2a3a1
Kavya-rajkumar99 <kavyarajkumar99@gmail.com>
2022-10-19 17:05:17 +0530

    ecommerce - added routes , header and herosection components

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeroSection from './components/HeroSection'
+
+const About = () => {
+  const data = "Cosmo Ecommerce"
+  return <HeroSection data = {data} />
+  
+}
+
+export default About
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeroSection from './components/HeroSection';
+
+const Home = () => {
+  const data = "Cosmo Store"
+  return <HeroSection data={data} />
+  
+}
+
+export default Home
