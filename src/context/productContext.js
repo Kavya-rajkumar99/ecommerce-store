057b0edadb8d91ef51ff98f4fe038631ec2dd697
Kavya-rajkumar99 <kavyarajkumar99@gmail.com>
2022-10-20 16:13:45 +0530

    added axios to fetch data from api

--- src/context/productContext.js
+++ src/context/productContext.js
@@ -1,13 +1,31 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
+import axios from "axios";
 
 const ProductContext = createContext();
 
-const ProductContextProvider = ({children}) => {
-    return <ProductContext.Provider value={"Kavya"}>{children}</ProductContext.Provider>
-}
+const API = "https://api.pujakaitem.com/api/products"
 
-const useProviderContext = () =>{
-    return useContext(ProductContext)
-}
+const ProductContextProvider = ({ children }) => {
 
-export {ProductContextProvider,useProviderContext}
+  const getProducts = async (url) => {
+    const response = await axios.get(url);
+    console.log(response)
+    const products = await response.data;
+  };
+
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+
+  return (
+    <ProductContext.Provider value={"Kavya"}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+const useProviderContext = () => {
+  return useContext(ProductContext);
+};
+
+export { ProductContextProvider, useProviderContext };
