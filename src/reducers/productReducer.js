b22a5441eb0a83d9f25d9bd5fcf46bbda0aeb13b
Kavya-rajkumar99 <kavyarajkumar99@gmail.com>
2022-10-20 18:29:58 +0530

    added state management for products with useReducer

---
+++ src/reducers/productReducer.js
@@ -0,0 +1,24 @@
+const productReducer = (state,action) =>{
+   switch(action.type){
+    case PRODUCTS_FETCH_REQUEST : return {
+        ...state,
+        isLoading : true
+    }
+    case PRODUCTS_FETCH_SUCCESS : 
+     const featuredProducts = action.payload.filter((product)=>product.featured)
+    return {
+        ...state,
+        isLoading : false,
+        products : action.payload,
+        featuredProducts 
+    }
+    case PRODUCTS_FETCH_REQUEST : return {
+        ...state,
+        isLoading : false,
+        isError : true
+    }
+    default : return state
+   }
+}
+
+export default productReducer
