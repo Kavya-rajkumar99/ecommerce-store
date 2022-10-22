e038747b9f61df24546650512384520ada0e3fd4
Kavya-rajkumar99 <kavyarajkumar99@gmail.com>
2022-10-22 20:19:29 +0530

    added products page

--- src/Products.js
+++ src/Products.js
@@ -1,8 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import FilterSection from "./components/FilterSection";
+import ProductList from "./components/ProductList";
+import Sort from "./components/Sort";
+import { useFilterContext } from "./context/filterContext";
 
 const Products = () => {
-  return <div>Products</div>;
+  const {filteredProducts} = useFilterContext();
+  console.log(filteredProducts)
+  return <Wrapper>
+    <div className="container grid grid-filter-column">
+      <div>
+        <FilterSection />
+      </div>
+      <section className="product-view--sort">
+        <div>
+          <Sort />
+        </div>
+        <div className="main-product">
+          <ProductList />
+        </div>
+      </section>
+    </div>
+  </Wrapper>;
 };
 
 const Wrapper = styled.section`
