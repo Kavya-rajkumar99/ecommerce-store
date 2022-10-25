99d7d5731346fbeaa8bf2b080912fff5d23bbcdc
Kavya-rajkumar99 <kavyarajkumar99@gmail.com>
2022-10-25 19:35:56 +0530

    added search box in filter section

--- src/components/FilterSection.js
+++ src/components/FilterSection.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+import { useFilterContext } from '../context/filterContext'
 
 const FilterSection = () => {
+  const {filters : {searchText} , updateFilterValue} = useFilterContext()
   return (
-    <div>
-      FilterSection
-    </div>
+    <Wrapper>
+      <div>
+        <form onSubmit={e => e.preventDefault()}>
+          <input type="text" name="searchText" value={searchText} onChange={updateFilterValue}/>
+        </form>
+      </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+padding : 9rem 0;
+`
 
 export default FilterSection
